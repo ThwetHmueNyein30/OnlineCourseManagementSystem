0ebab7734e02c0a207387e9e2a77223d51beaa7b
ThwetHumeNyein30 <thwethmuenyein.thn@gmail.com>
2022-05-09 14:01:30 +0630

    create course registration service

--- src/main/java/com/thn/onlinecoursemanagement/controller/CourseRegistrationController.java
+++ src/main/java/com/thn/onlinecoursemanagement/controller/CourseRegistrationController.java
@@ -7,6 +7,7 @@ import com.thn.onlinecoursemanagement.payload.resquest.CourseRegisterRequestBody
 import com.thn.onlinecoursemanagement.constant.RoleEnum;
 import com.thn.onlinecoursemanagement.payload.resquest.SMSRequestBody;
 import com.thn.onlinecoursemanagement.repositories.*;
+import com.thn.onlinecoursemanagement.services.CourseRegistrationService;
 import com.thn.onlinecoursemanagement.services.SMSService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,75 +31,15 @@ public class CourseRegistrationController {
 
     @Autowired
     CourseRegistrationRepository courseRegistrationRepository;
+
     @Autowired
-    RoleRepository roleRepository;
-    @Autowired
-    PersonRepository personRepository;
-    @Autowired
-    CourseRepository courseRepository;
-    @Autowired
-    SMSService smsService;
-    @Autowired
-    EWalletRepository ewalletRepository;
+    CourseRegistrationService courseRegistrationService;
 
 
     @PostMapping()
     BaseResponse registerCourse(@RequestBody CourseRegisterRequestBody courseRegisterRequestBody) {
-        BaseResponse response = new BaseResponse();
-        response.setStatus(true);
-        response.setDateTime(LocalDateTime.now());
 
-        Optional<Person> optionalPerson=personRepository.findById(courseRegisterRequestBody.getStudentId());
-        if (!optionalPerson.isPresent()) {
-            response.setMessage("Don't have any person for that id!");
-            return response;
-        }
-
-        Optional<Course> optionalCourse=courseRepository.findById(courseRegisterRequestBody.getCourseId());
-        if (!optionalCourse.isPresent()) {
-            response.setMessage("Don't have any course for that id!");
-            return response;
-        }
-        Person person=optionalPerson.get();
-        Course course=optionalCourse.get();
-
-        if (LocalDateTime.now().isBefore(course.getRegisteredFrom()) || LocalDateTime.now().isAfter(course.getRegisteredTo())) {
-            response.setMessage("Cannot Register within this time!");
-            return response;
-        }
-        Optional<Role> optionalRole = roleRepository.findByRoleId(person.getRoleId());
-
-        if (!optionalRole.isPresent()) {
-            response.setMessage("Don't have any role for that role id!");
-            return response;
-        }
-
-        String roleName = optionalRole.get().getName();
-        if (!roleName.equals(RoleEnum.STUDENT_ROLE.name())) {
-            response.setMessage("Only student can register!");
-            return response;
-        }
-
-        if(courseRegisterRequestBody.getFee()< course.getFee()){
-            response.setMessage("Not Enough money to register");
-        }
-
-        if(Objects.equals(courseRegisterRequestBody.getFee(), course.getFee())){
-            response.setMessage("Need to pay the correct amount!");
-        }
-
-        CourseRegistration courseRegistration = new CourseRegistration(person.getId(), course.getId(), course.getFee());
-        courseRegistrationRepository.save(courseRegistration);
-        response.setResult(courseRegistration);
-        response.setMessage("Successfully registered");
-
-        //deduct fee from person's eWallet
-        ewalletRepository.deductBalance(person.getId(), course.getId());
-
-        String message= String.format("Thanks for registration this course. You already register  %s, with phone %s .",course.getName(),person.getPhone());
-        String result= smsService.createSMS(new SMSRequestBody(SMS_ADDRESS,person.getPhone(),message));
-        log.info("result: {} ",result);
-        return response;
+        return courseRegistrationService.validateRegistration(courseRegisterRequestBody);
     }
 
 
