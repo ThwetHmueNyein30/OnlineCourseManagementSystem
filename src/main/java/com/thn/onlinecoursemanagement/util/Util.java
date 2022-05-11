4bd4cbd7843214fd84f8fffade7b854474e5c321
ThwetHumeNyein30 <thwethmuenyein.thn@gmail.com>
2022-05-11 16:55:05 +0630

    Commit for Util class and changing in imp

---
+++ src/main/java/com/thn/onlinecoursemanagement/util/Util.java
@@ -0,0 +1,32 @@
+package com.thn.onlinecoursemanagement.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author ThwetHmueNyein
+ * @Date 09/05/2022
+ */
+@Slf4j
+public class Util {
+
+     public static String encodeFileToBase64Binary(String imgUrl) {
+        try {
+            File file = new File(imgUrl);
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStreamReader.read(bytes);
+            log.info("Encoded String: {} ", new String(Base64.encodeBase64(bytes), StandardCharsets.UTF_8));
+            return new String(Base64.encodeBase64(bytes), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.info("Exception : ", e);
+        }
+        return null;
+    }
+
+
+}
