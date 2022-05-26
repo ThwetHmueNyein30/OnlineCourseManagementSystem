791758f37ebf4423a38184458b149b7d5aca0abb
Cao Thanh Tùng <caothanhtung@mytel.com.mm>
2022-05-26 10:36:32 +0630

    [move] ewallet query to config file

--- src/main/java/com/thn/onlinecoursemanagement/config/AppConfig.java
+++ src/main/java/com/thn/onlinecoursemanagement/config/AppConfig.java
@@ -12,4 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "config")
 public class AppConfig {
     String uploadFolder;
+    EWallet eWallet;
+    public static class EWallet{
+        private String infoQuery;
+        private String updateQuery;
+        private String insertQuery;
+        private String historyQuery;
+        private String historyInsert;
+    }
 }
